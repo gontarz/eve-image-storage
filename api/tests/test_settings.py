b2f581fca556d445f70b3ccfc53a7c08b74609e8
eustachy <kylo.997@wp.pl>
2019-01-31 22:55:49 +0100

    test

--- api/tests/test_settings.py
+++ api/tests/test_settings.py
@@ -2,26 +2,51 @@
 """
 """
 
-from .. settings import _MONGO_DBNAME_TEST
+from ..settings import _MONGO_DBNAME_TEST, _COLLECTION, _RAW_IMAGE_ROUTE, _FILE_KEY
 
 test_sets = {
-            'MONGO_HOST': 'localhost',
-            'MONGO_PORT': 27017,
-            'MONGO_DBNAME': _MONGO_DBNAME_TEST,
-            'RESOURCE_METHODS': ['GET', 'POST'],
-            'DOMAIN': {
-                'images': {
-                    # 'resource_methods': ['GET', 'POST'],
-                    # 'allow_unknown': True,
-                    'schema': {
-                        'file_name': {
-                            'type': 'string'
-                        },
-                        'path': {
-                            'type': 'string'
-                        }
-                    }
-                }
+    'DOMAIN': {
+        'images': {
+            'resource_methods': ['GET'],
+            # 'item_methods': ['GET'],
+            # 'allow_unknown': True,
+            # 'datasource': {
+            #     'projection': {
+            #         'md5': 0,
+            #         'original_filename': 0
+            #     }
+            # },
+            'additional_lookup': {
+                'url': 'regex("[\w]+")',
+                'field': 'file_name'
             },
-            '_SECRET_KEY': 'xyz'
+            'schema': {
+                'md5': {
+                    'type': 'string'
+                },
+                'original_filename': {
+                    'type': 'string'
+                },
+                'upload_date': {
+                    'type': 'datetime'
+                },
+                'file_name': {
+                    'type': 'string'
+                },
+                'path': {
+                    'type': 'string'
+                }
+            }
         }
+    },
+    'MONGO_HOST': 'localhost',
+    'MONGO_PORT': 27017,
+    'MONGO_DBNAME': _MONGO_DBNAME_TEST,
+    'RESOURCE_METHODS': ['GET', 'POST'],
+
+    '_SECRET_KEY': 'xyz',
+    '_COLLECTION': _COLLECTION,
+    '_RAW_IMAGE_ROUTE': _RAW_IMAGE_ROUTE,
+    '_FILE_KEY': _FILE_KEY
+
+}
